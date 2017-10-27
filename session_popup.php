32cd95587838d94c2e147e03ce337315409e56ab
Aswan Rezpector <aswan.peterpan@gmail.com>
2017-10-27 21:46:53 +0800

    Add files via upload

---
+++ session_popup.php
@@ -0,0 +1,25 @@
+<?php
+	// Include file koneksi
+	include"../lib/conn.php";
+	include"../lib/zip.php";
+	
+	session_start();// Memulai Session
+	// Menyimpan Session
+	$user_check = $_SESSION['login_user'];
+	// Ambil nama karyawan berdasarkan username karyawan dengan mysql_fetch_assoc
+	$ses_sql=mysqli_query($link, "select nama_lengkap from user where usernm = '$user_check'");
+	$row = mysqli_fetch_assoc($ses_sql);
+	$login_session = $row['nama_lengkap'];
+
+	//data header
+	$HEAD = array();
+	$sqlhead = mysqli_query($link, "SELECT * FROM tb_pengaturan");
+	while ($p = mysqli_fetch_assoc($sqlhead)) {
+		$HEAD[$p['nama_peng']] = $p['val_peng'];
+	}
+
+	if(!isset($login_session)){
+		mysqli_close($link); // Menutup koneksi
+		header('location: logout.php'); // Mengarahkan ke Home Page
+	}
+?>
